1e1e1c3e09378b4770adf19f0dbe6edcffccc538
rch141 <rch141@uclive.ac.nz>
2018-10-12 13:01:46 +1300

    rotation and placing ship

--- gamefiles/setupGame.c
+++ gamefiles/setupGame.c
@@ -3,6 +3,8 @@
 #include "tinygl.h"
 #include "stdio.h"
 #include "navswitch.h"
+#include "button.h"
+#include "pio.h"
 
 typedef struct ship_s Ship;
 struct ship_s {
@@ -11,6 +13,8 @@ struct ship_s {
     int x2;
     int y2;
 };
+
+
 
 Ship createShip(int x1, int y1, int x2, int y2) {
     Ship ship = {x1, y1, x2, y2};
@@ -34,31 +38,6 @@ static int checkY(int y1, int y2) {
     else if (y1 > 6 || y2 > 6) {return 0;}
     else {return 1;}
 }
-
-/*
-static void moveShipNorth(Ship *ship)
-{
-    makeShip(*ship, 0);
-
-    if (!checkY(ship->y1 - 1)) {
-        ship->y1--;
-    }
-    if (!checkY(ship->y2 - 1)) {
-        ship->y2--;
-    }
-    makeShip(*ship, 1);
-}
-
-static void moveShipSouth(Ship *ship)
-{
-    makeShip(*ship, 0);
-
-//    if (checkBox(ship.y1 - 1))
-    ship->y1++;
-    ship->y2++;
-    makeShip(*ship, 1);
-}
-*/
 
 static void moveShipNorth(Ship *ship)
 {
@@ -108,28 +87,49 @@ static void moveShipWest(Ship *ship)
     makeShip(*ship, 1);
 }
 
-int main (void)
+void rotateShip(Ship *ship, int sizeShip)
+{
+    makeShip(*ship, 0);
+    if (ship->x1 != ship->x2) {
+        ship->x1 = 2;
+        ship->y1 = 2;
+        ship->x2 = 2;
+        ship->y2 = sizeShip + 1;
+    } else if (ship->y1 != ship->y2) {
+        ship->x1 = 1;
+        ship->y1 = 3;
+        ship->x2 = sizeShip;
+        ship->y2 = 3;
+    }
+    makeShip(*ship, 1);
+}
+
+/*
+void getCoord(Ship ship, int[] coords) {
+
+}
+*/
+
+void setup (int* toReturn, int sizeShip)
 {
     system_init ();
     pacer_init (1000);
     tinygl_init (1000);
     navswitch_init ();
-
-    Ship ship = createShip(1, 1, 4, 1);
-    //Ship ship = createShip(0, 0, 0, 3);
-
-
+    button_init();
+    Ship ship = createShip(0, 1, 0, sizeShip);
     makeShip(ship, 1);
+
     while (1)
         {
             pacer_wait ();
             tinygl_update ();
             navswitch_update ();
+            button_update ();
 
             if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
                 moveShipNorth(&ship);
             }
-
 
             if (navswitch_push_event_p (NAVSWITCH_SOUTH)) {
                 moveShipSouth(&ship);
@@ -143,5 +143,20 @@ int main (void)
                 moveShipWest(&ship);
             }
 
+            if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+                rotateShip(&ship, sizeShip);
+            }
+            if (button_push_event_p (0)) {
+                makeShip(ship, 0);
+                toReturn[0] = ship.x1; toReturn[1] = ship.y1;
+                toReturn[2] = ship.x2; toReturn[3] = ship.y2;
+                return toReturn;
+            }
+
         }
+}
+int main(void)
+{
+    int test[4];
+    setup(test, 4);
 }
